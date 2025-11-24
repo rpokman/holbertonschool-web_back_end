6b3afffa1da2feecc747c43191b28ae108135cad
rpokman <roymathis74200@gmail.com>
2025-11-24 13:17:58 +0100

    docs: Add module docstring to 1-concat.py.

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Module 1-concat
+Contains a function that concatenates two strings
+"""
+
 def concat(str1: str, str2: str) -> str:
     """
     Takes a string str1 and a string str2 as arguments and returns

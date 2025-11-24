bb3d05e79547a1188a2183aefeefb7b25086e9ef
rpokman <roymathis74200@gmail.com>
2025-11-24 12:41:57 +0100

    feat: add type annotations and docstrings to the `add` function.

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
+"""
+Module 0-add
+Contains a function that adds two floats
+"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    Adds two floats
+    Args:
+        a: first float
+        b: second float
+    Returns:
+        The sum of a and b
+    """
     return a + b

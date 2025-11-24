4713540b10ddc15705275e1a92b00b4c126e3226
rpokman <roymathis74200@gmail.com>
2025-11-24 13:19:51 +0100

    style: Add blank line after module docstring for improved readability.

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,6 +4,7 @@ Module 1-concat
 Contains a function that concatenates two strings
 """
 
+
 def concat(str1: str, str2: str) -> str:
     """
     Takes a string str1 and a string str2 as arguments and returns

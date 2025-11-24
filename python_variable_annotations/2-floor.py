04bc385f5d8af1ed53c56b818a134795723bb3dd
rpokman <roymathis74200@gmail.com>
2025-11-24 14:07:52 +0100

    feat: add `floor` function with type annotations and a test script.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module 2-floor
+Contains a function that returns the floor of a float
+"""
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float
+    Args:
+        n: float
+    Returns:
+        The floor of n
+    """
+    return int(n)

0ad92891e44a819be69983692379babb4b9ce477
rpokman <roymathis74200@gmail.com>
2025-11-24 14:19:46 +0100

    feat: add `to_str` function with type annotations to convert a float to its string representation.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module 3-to_str
+Contains a function that returns the string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the float n
+    Args:
+        n: float
+    Returns:
+        The string representation of n
+    """
+    return str(n)

11303830e25b2982ba914318b320b8e90ae09d82
rpokman <roymathis74200@gmail.com>
2025-11-26 13:12:21 +0100

    fix: update docstring in `to_kv` function for clarity and accuracy

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -8,8 +8,8 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Takes a string k and an int OR float v as arguments and returns a tuple.
+    Takes a string k and an int OR float v as arguments.
     The first element of the tuple is the string k.
-    The second element is the square of the int/float v and should be annotated as a float.
+    The second element is the square of the int.
     """
     return (k, v * v)
